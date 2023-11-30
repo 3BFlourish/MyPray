174e356ac073a945c50b7120076cf0d3f9120779
Minh Tran <minht@bap.jp>
2023-11-30 23:48:02 +0700

    demo

---
+++ .output/public/_nuxt/default.ff0071f5.js
@@ -0,0 +1,1 @@
+import{_ as t,q as r,L as n,m as o}from"./entry.e20c752d.js";const s={};function a(e,c){return o(),r("div",null,[n(e.$slots,"default")])}const _=t(s,[["render",a]]);export{_ as default};
